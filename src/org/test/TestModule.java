b179a57eb4c67652ddeff87aa7befed17a975561
IMKavink <kavinkumar8675@gmail.com>
2021-08-17 09:11:21 +0530

    new

---
+++ src/org/test/TestModule.java
@@ -0,0 +1,26 @@
+package org.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+public class TestModule {
+	
+ private void tc() {
+	System.out.println("ghaethteh");
+
+}
+ private void tc1() {
+	
+System.out.println("grwehgetj");
+}
+ public static void main(String[] args) {
+	 TestModule t=new TestModule();
+	 t.tc();
+	 t.tc1();
+}
+
+}
